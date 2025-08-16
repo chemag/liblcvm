30d6f621e609f7633caecfb8e32b35f311feb5fb
Chema Gonzalez <chemag@meta.com>
2025-08-15 19:18:37 -0700

    liblcvm: add parsing unittest
    Tested:
    ```
    $ ./test/liblcvm_unittest
    Running main() from /builddir/build/BUILD/gtest-1.15.2-build/googletest-1.15.2/googletest/src/gtest_main.cc
    [==========] Running 1 test from 1 test suite.
    [----------] Global test environment set-up.
    [----------] 1 test from LiblcvmTest
    [ RUN      ] LiblcvmTest.TestParserPolicy
    [       OK ] LiblcvmTest.TestParserPolicy (2 ms)
    [----------] 1 test from LiblcvmTest (2 ms total)
    
    [----------] Global test environment tear-down
    [==========] 1 test from 1 test suite ran. (2 ms total)
    [  PASSED  ] 1 test.
    ```

--- test/liblcvm_unittest.cc
+++ test/liblcvm_unittest.cc
@@ -6,6 +6,27 @@
 #include <gtest/gtest.h>
 #include <liblcvm.h>  // for various
 
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+using ::testing::ElementsAreArray;
+
+namespace {
+std::string readFileToString(const std::string& filename) {
+  std::ifstream file(filename, std::ios::in | std::ios::binary);
+  if (!file) {
+    throw std::runtime_error("Could not open file: " + filename);
+  }
+
+  std::ostringstream contents;
+  contents << file.rdbuf();
+  return contents.str();
+}
+}  // namespace
+
 namespace liblcvm {
 
 class LiblcvmTest : public ::testing::Test {
@@ -14,7 +35,138 @@ class LiblcvmTest : public ::testing::Test {
   ~LiblcvmTest() override {}
 };
 
-TEST_F(LiblcvmTest, TestCalculateMedianAbsoluteDeviation) {
-  ASSERT_NE(0, 1) << "Broken Check";
+TEST_F(LiblcvmTest, TestParserPolicy) {
+  // 1. set input files
+  std::string input_filename = "MOV1.MOV";
+  std::string infile = std::string(TEST_MEDIA_DIR) + "/" + input_filename;
+  std::string policy_filename = "example.txt";
+  std::string policy_infile =
+      std::string(TEST_POLICY_DIR) + "/" + policy_filename;
+  std::string policy = readFileToString(policy_infile);
+
+  // 2. set parsing parameters
+  auto liblcvm_config = std::make_unique<LiblcvmConfig>();
+  liblcvm_config->set_sort_by_pts(true);
+  liblcvm_config->set_policy(policy);
+  liblcvm_config->set_debug(1);
+
+  // 3. parse the input files
+  LiblcvmKeyList keys;
+  LiblcvmValList vals;
+  LiblcvmKeyList keys_timing;
+  LiblcvmTimingList vals_timing;
+  bool calculate_timestamps = true;
+  ASSERT_EQ(0, IsobmffFileInformation::parse_to_lists(
+                   infile.c_str(), *liblcvm_config, &keys, &vals,
+                   calculate_timestamps, &keys_timing, &vals_timing))
+      << "error: IsobmffFileInformation::parse_to_map() in " << infile;
+
+  // 4. check the main parsing values
+  LiblcvmKeyList expected_keys = {
+      "infile",
+      "filesize",
+      "bitrate_bps",
+      "width",
+      "height",
+      "type",
+      "horizresolution",
+      "vertresolution",
+      "depth",
+      "chroma_format",
+      "bit_depth_luma",
+      "bit_depth_chroma",
+      "video_full_range_flag",
+      "colour_primaries",
+      "transfer_characteristics",
+      "matrix_coeffs",
+      "num_video_frames",
+      "frame_rate_fps_median",
+      "frame_rate_fps_average",
+      "frame_rate_fps_reverse_average",
+      "frame_rate_fps_stddev",
+      "video_freeze",
+      "audio_video_ratio",
+      "duration_video_sec",
+      "duration_audio_sec",
+      "timescale_video_hz",
+      "timescale_audio_hz",
+      "pts_duration_sec_average",
+      "pts_duration_sec_median",
+      "pts_duration_sec_stddev",
+      "pts_duration_sec_mad",
+      "frame_drop_count",
+      "frame_drop_ratio",
+      "normalized_frame_drop_average_length",
+      "frame_drop_length_percentile_50",
+      "frame_drop_length_percentile_90",
+      "frame_drop_length_consecutive_2",
+      "frame_drop_length_consecutive_5",
+      "num_video_keyframes",
+      "key_frame_ratio",
+      "audio_type",
+      "channel_count",
+      "sample_rate",
+      "sample_size",
+      "warn_list",
+      "error_list",
+  };
+  EXPECT_EQ(expected_keys, keys) << "output keys incorrect";
+  // check the file name
+  std::string fullpath = std::get<std::string>(vals[0]);
+  std::filesystem::path p(fullpath);
+  std::string filename = p.filename().string();
+  std::string expected_filename = input_filename;
+  EXPECT_EQ(expected_filename, filename) << "incorrect filename";
+  // check the remaining values
+  LiblcvmValList expected_vals = {
+      17784,
+      13455.7373046875,
+      1920.0,
+      1080.0,
+      std::string("hvc1"),
+      4718592,
+      4718592,
+      24,
+      1,
+      8,
+      8,
+      0,
+      1,
+      1,
+      1,
+      634,
+      59.999996185302734,
+      59.965530395507812,
+      59.962047576904297,
+      0.43256726861000061,
+      0,
+      1.0058075189590454,
+      10.573333740234375,
+      10.634738922119141,
+      600,
+      44100,
+      0.016677215695381165,
+      0.01666666753590107,
+      0.00013217281957622617,
+      0.0,
+      0,
+      0.0,
+      0.0,
+      0.0,
+      0.0,
+      0,
+      0,
+      11,
+      57.636363983154297,
+      std::string("mp4a"),
+      1,
+      44100,
+      16,
+      std::string("Suspicious bitrate_bps;Suspicious depth"),
+      std::string("Invalid bitrate_bps;Invalid width;Invalid height;Invalid "
+                  "type;Invalid audio_video_ratio"),
+  };
+  EXPECT_THAT(expected_vals, ElementsAreArray(vals.begin() + 1, vals.end()))
+      << "output vals incorrect";
 }
 }  // namespace liblcvm
