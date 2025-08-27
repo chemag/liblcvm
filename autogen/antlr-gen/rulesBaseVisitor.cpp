c514f09119408c0494e2ba3a9b1c5e7842664cf7
Marko Panayotov <panayotovmarko@meta.com>
2025-08-27 10:28:36 +0300

    liblcvm: add 'autogen', 'antlr-gen' and 'protobuff-gen' dirs

---
+++ autogen/antlr-gen/rulesBaseVisitor.cpp
@@ -0,0 +1,7 @@
+
+// Generated from /Users/panayotovmarko/Work/liblcvm/policy/rules.g4 by ANTLR 4.13.1
+
+
+#include "rulesBaseVisitor.h"
+
+
---
+++ autogen/antlr-gen/rulesLexer.h
@@ -0,0 +1,51 @@
+
+// Generated from /Users/panayotovmarko/Work/liblcvm/policy/rules.g4 by ANTLR 4.13.1
+
+#pragma once
+
+
+#include "antlr4-runtime.h"
+
+
+
+
+class  rulesLexer : public antlr4::Lexer {
+public:
+  enum {
+    OR = 1, AND = 2, NOT = 3, IN = 4, RANGE = 5, EQ = 6, NE = 7, LT = 8, 
+    GT = 9, LE = 10, GE = 11, LPAREN = 12, RPAREN = 13, COMMA = 14, WARN = 15, 
+    ERROR = 16, IDENT = 17, NUMBER = 18, STRING = 19, WS = 20
+  };
+
+  explicit rulesLexer(antlr4::CharStream *input);
+
+  ~rulesLexer() override;
+
+
+  std::string getGrammarFileName() const override;
+
+  const std::vector<std::string>& getRuleNames() const override;
+
+  const std::vector<std::string>& getChannelNames() const override;
+
+  const std::vector<std::string>& getModeNames() const override;
+
+  const antlr4::dfa::Vocabulary& getVocabulary() const override;
+
+  antlr4::atn::SerializedATNView getSerializedATN() const override;
+
+  const antlr4::atn::ATN& getATN() const override;
+
+  // By default the static state used to implement the lexer is lazily initialized during the first
+  // call to the constructor. You can call this function if you wish to initialize the static state
+  // ahead of time.
+  static void initialize();
+
+private:
+
+  // Individual action functions triggered by action() above.
+
+  // Individual semantic predicate functions triggered by sempred() above.
+
+};
+
